a1342a9e676708ece11b8244ab2da6916277963f
esteban <esteban.quinones@outlook.fr>
2022-05-23 14:44:23 +0200

    reset after bug

--- js/graph.js
+++ js/graph.js
@@ -3,41 +3,100 @@ window.onload = function () {
     var chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         title:{
-            text: "Company Revenue by Year"
+            text: "Les réseaux sociaux"
         },
-        axisY: {
-            title: "Revenue in USD",
-            valueFormatString: "#0,,.",
-            suffix: "mn",
+        axisY :{
+            includeZero: false,
             prefix: "$"
+        },
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            fontSize: 13
         },
         data: [{
             type: "splineArea",
-            color: "rgba(54,158,173,.7)",
-            markerSize: 5,
-            xValueFormatString: "YYYY",
-            yValueFormatString: "$#,##0.##",
+            showInLegend: true,
+            name: "Instagram",
+            yValueFormatString: "$#,##0",
+            xValueFormatString: "MMM YYYY",
             dataPoints: [
-                { x: new Date(2000, 0), y: 3289000 },
-                { x: new Date(2001, 0), y: 3830000 },
-                { x: new Date(2002, 0), y: 2009000 },
-                { x: new Date(2003, 0), y: 2840000 },
-                { x: new Date(2004, 0), y: 2396000 },
-                { x: new Date(2005, 0), y: 1613000 },
-                { x: new Date(2006, 0), y: 2821000 },
-                { x: new Date(2007, 0), y: 2000000 },
-                { x: new Date(2008, 0), y: 1397000 },
-                { x: new Date(2009, 0), y: 2506000 },
-                { x: new Date(2010, 0), y: 2798000 },
-                { x: new Date(2011, 0), y: 3386000 },
-                { x: new Date(2012, 0), y: 6704000},
-                { x: new Date(2013, 0), y: 6026000 },
-                { x: new Date(2014, 0), y: 2394000 },
-                { x: new Date(2015, 0), y: 1872000 },
-                { x: new Date(2016, 0), y: 2140000 }
+                { x: new Date(2016, 2), y: 30000 },
+                { x: new Date(2016, 3), y: 35000 },
+                { x: new Date(2016, 4), y: 30000 },
+                { x: new Date(2016, 5), y: 30400 },
+                { x: new Date(2016, 6), y: 20900 },
+                { x: new Date(2016, 7), y: 31000 },
+                { x: new Date(2016, 8), y: 30200 },
+                { x: new Date(2016, 9), y: 30000 },
+                { x: new Date(2016, 10), y: 33000 },
+                { x: new Date(2016, 11), y: 38000 },
+                { x: new Date(2017, 0),  y: 38900 },
+                { x: new Date(2017, 1),  y: 39000 }
+            ]
+         },
+        {
+            type: "splineArea", 
+            showInLegend: true,
+            name: "Facebook",
+            yValueFormatString: "$#,##0",
+            dataPoints: [
+                { x: new Date(2016, 2), y: 20100 },
+                { x: new Date(2016, 3), y: 16000 },
+                { x: new Date(2016, 4), y: 14000 },
+                { x: new Date(2016, 5), y: 18000 },
+                { x: new Date(2016, 6), y: 18000 },
+                { x: new Date(2016, 7), y: 21000 },
+                { x: new Date(2016, 8), y: 22000 },
+                { x: new Date(2016, 9), y: 25000 },
+                { x: new Date(2016, 10), y: 23000 },
+                { x: new Date(2016, 11), y: 25000 },
+                { x: new Date(2017, 0), y: 26000 },
+                { x: new Date(2017, 1), y: 25000 }
+            ]
+         },
+        {
+            type: "splineArea", 
+            showInLegend: true,
+            name: "Twitter",
+            yValueFormatString: "$#,##0",     
+            dataPoints: [
+                { x: new Date(2016, 2), y: 10100 },
+                { x: new Date(2016, 3), y: 6000 },
+                { x: new Date(2016, 4), y: 3400 },
+                { x: new Date(2016, 5), y: 4000 },
+                { x: new Date(2016, 6), y: 9000 },
+                { x: new Date(2016, 7), y: 3900 },
+                { x: new Date(2016, 8), y: 4200 },
+                { x: new Date(2016, 9), y: 5000 },
+                { x: new Date(2016, 10), y: 14300 },
+                { x: new Date(2016, 11), y: 12300 },
+                { x: new Date(2017, 0), y: 8300 },
+                { x: new Date(2017, 1), y: 6300 }
+            ]
+         },
+        {
+            type: "splineArea", 
+            showInLegend: true,
+            yValueFormatString: "$#,##0",      
+            name: "Tik Tok",
+            dataPoints: [
+                { x: new Date(2016, 2), y: 1700 },
+                { x: new Date(2016, 3), y: 2600 },
+                { x: new Date(2016, 4), y: 1000 },
+                { x: new Date(2016, 5), y: 1400 },
+                { x: new Date(2016, 6), y: 900 },
+                { x: new Date(2016, 7), y: 1000 },
+                { x: new Date(2016, 8), y: 1200 },
+                { x: new Date(2016, 9), y: 5000 },
+                { x: new Date(2016, 10), y: 1300 },
+                { x: new Date(2016, 11), y: 2300 },
+                { x: new Date(2017, 0), y: 2800 },
+                { x: new Date(2017, 1), y: 1300 }
             ]
         }]
-        });
+    });
     chart.render();
     
     }
